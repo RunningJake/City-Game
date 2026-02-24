1d0a0bf36013b66b451f8a495031111e170b624d
RunningJake <CottrillJake@gmail.com>
2026-02-23 21:02:20 -0500

    Initialize Firebase configuration and database

---
+++ firebase.js
@@ -0,0 +1,12 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyCXjYrXjXo5zHBTtfwvfbZHH-TksGEzYlc",
+  authDomain: "city-game-d5837.firebaseapp.com",
+  databaseURL: "https://city-game-d5837-default-rtdb.firebaseio.com",
+  projectId: "city-game-d5837",
+  storageBucket: "city-game-d5837.firebasestorage.app",
+  messagingSenderId: "951479993023",
+  appId: "1:951479993023:web:a32cb80c124882c3ffbc0a"
+};
+
+firebase.initializeApp(firebaseConfig);
+const db = firebase.database();
